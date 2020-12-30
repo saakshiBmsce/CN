856998b9891901b37605bddacbfe7c589c4d1d47
saakshiBmsce <53811241+saakshiBmsce@users.noreply.github.com>
2020-12-30 10:47:25 +0530

    Add files via upload

---
+++ TCP_client.py
@@ -0,0 +1,12 @@
+from socket import *
+serverName = 'DESKTOP-KG33ENC'
+serverPort = 13001
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+sentence = input("Enter file name")
+
+clientSocket.send(sentence.encode())
+filecontents = clientSocket.recv(1024).decode()
+print ('From Server:', filecontents)
+clientSocket.close()
+
---
+++ TCP_server.py
@@ -0,0 +1,17 @@
+from socket import *
+serverName='DESKTOP-KG33ENC'
+serverPort = 13001
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind((serverName,serverPort))
+serverSocket.listen(1)
+print ("The server is ready to receive")
+while 1:
+    connectionSocket, addr = serverSocket.accept()
+    sentence = connectionSocket.recv(1024).decode()
+
+    file=open(sentence,"r")
+    l=file.read(1024)
+
+    connectionSocket.send(l.encode())
+    file.close()
+    connectionSocket.close()
